55a3d6617c31d585e9e95abf9c25b6ac8a7ea7fd
renzhenwang <46158457+renzhenwang@users.noreply.github.com>
2020-10-29 16:10:02 +0800

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -2,9 +2,9 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset in ['lits', 'pairwise_lits']:
-            return '../data/minor_lits/'                            
+            return '../lits/minor_lits/'                            
         elif dataset in ['chaos', 'pairwise_chaos']:
-            return '../data/cross_chaos_5/'
+            return '../chaos/cross_chaos_5/'
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
